434b79116e4b5f50ffa24f2306f1f55993cfb218
Pravin Singh Rana <dev.psrana@gmail.com>
2025-03-16 16:50:59 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button >This is a button </Button>
+     
+    </div>
+  )
+}
+
+export default App
